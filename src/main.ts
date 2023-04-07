a0814e647036310b4142848d5b1f09c3a9a4e920
iacobucci <iacobuccivalerio@gmail.com>
2023-04-07 11:53:40 +0200

    modified:   buildspec.yml 	modified:   package-lock.json 	modified:   package.json 	modified:   src/main.ts 	modified:   tsconfig.json 	renamed:    bootstrap.sh -> util/bootstrap.sh 	renamed:    create_zip.sh -> util/create_zip.sh 	renamed:    tunnel.sh -> util/tunnel.sh

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { GetObjectCommand, PutObjectCommand, ListObjectsCommand, S3Client } from
 import express, { Request, Response } from "express";
 import path from "path";
 import dotenv from "dotenv";
-import mysql from "mysql";
+import mysql, { MysqlError } from "mysql";
 import { v4 as uuidv4 } from 'uuid';
 
 const REGION = "eu-north-1";
@@ -11,9 +11,7 @@ const s3Client = new S3Client({ region: REGION });
 const app = express();
 const port = process.env.PORT || 3000;
 
-import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const dirname = path.resolve();
 
 dotenv.config();
 // load environment variables from /var/www/efs/.env if it exists
@@ -37,7 +35,7 @@ async function connectToDb(): Promise<void> {
       password: process.env.RDS_PASSWORD,
       database: process.env.RDS_DATABASE,
     });
-    dbconn.connect((err) => {
+    dbconn.connect((err : MysqlError) => {
       if (err) {
         reject(err);
       } else {
@@ -50,7 +48,7 @@ async function connectToDb(): Promise<void> {
 
 connectToDb().then(() => {
   app.get("/", (req: Request, res: Response) => {
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile(dirname + "/dist" + "/index.html");
   });
 
   app.get("/create", (req: Request, res: Response) => {
@@ -69,7 +67,7 @@ connectToDb().then(() => {
     s3Client.send(new PutObjectCommand(params)).then(() => {
       console.log("new " + B);
 
-      dbconn.query("INSERT INTO node.tab1 (A,B) VALUES (?,?)", [A, B], (err, result, fields) => {
+      dbconn.query("INSERT INTO node.tab1 (A,B) VALUES (?,?)", [A, B], (err: any, result: any, fields: any) => {
         if (err) throw err;
 
         res.json(A);
@@ -84,7 +82,7 @@ connectToDb().then(() => {
     var A: string = req.query.id as string;
     var B: string;
 
-    dbconn.query("SELECT B FROM node.tab1 WHERE A = ?", [A], (err, result, fields) => {
+    dbconn.query("SELECT B FROM node.tab1 WHERE A = ?", [A], (err : any, result: any, fields: any) => {
       if (err) throw err;
       B = result[0].B;
       const params = {
@@ -94,8 +92,8 @@ connectToDb().then(() => {
 
       console.log("access " + B);
 
-      s3Client.send(new GetObjectCommand(params)).then((data) => {
-        data.Body.transformToByteArray().then((data) => {
+      s3Client.send(new GetObjectCommand(params)).then((data: any) => {
+        data.Body.transformToByteArray().then((data: any) => {
           var td: TextDecoder = new TextDecoder();
           res.json(td.decode(data));
         });
@@ -108,7 +106,7 @@ connectToDb().then(() => {
   });
 
   app.get("/list", (req: Request, res: Response) => {
-    dbconn.query("SELECT A FROM node.tab1", (err, result, fields) => {
+    dbconn.query("SELECT A FROM node.tab1", (err: any, result: any, fields: any) => {
       
       // inviare come json
       var tosend = [];
